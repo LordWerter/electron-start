bec5c343b7e12ec91a6372e1b7b5b86aecb9419c
Alexander Rudenko <Aleksandr.Rudenko@x5.ru>
2021-10-03 13:59:52 +0300

    first commit

--- packages/app__sst/src/index.tsx
+++ packages/app__sst/src/index.tsx
@@ -3,29 +3,12 @@
  */
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ThemeProvider } from '@emotion/react';
-
-import { App } from './epics';
-
-// #if process.env.IXR
-// @ts-ignore
-import { sst as theme } from '../../ui__themes/src/ixr/apps';
-// #endif
-// #if process.env.PEREK
-// @ts-ignore
-import { sst as theme } from '../../ui__themes/src/perekrestok/apps';
-/// #endif
-
-/**
- * imports Service Worker
- */
-// import registerServiceWorker from './registerServiceWorker';
 
 window.addEventListener('load', () => {
     ReactDOM.render(
-        <ThemeProvider theme={theme}>
-        <App />
-        </ThemeProvider>,
+        <div>
+            Hello from app#2
+        </div>,
         document.getElementById('root'),
     );
 });
--- packages/app__sst/webpack.config.ts
+++ packages/app__sst/webpack.config.ts
@@ -1,6 +1,4 @@
 import webpack from 'webpack';
-const ConditionalLoader = require.resolve('webpack-conditional-loader');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const configuration: webpack.Configuration = {
     entry: './src/index.tsx',
@@ -19,34 +17,6 @@ const configuration: webpack.Configuration = {
     module: {
         rules: [
             {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    {
-                        loader: ImageMinimizerPlugin.loader,
-                        options: {
-                            filter: (source: any) => {
-                                // The `source` argument is a `Buffer` of source file
-                                // The `sourcePath` argument is an absolute path to source
-                                if (source.byteLength < 8192) {
-                                    return false;
-                                }
-
-                                return true;
-                            },
-                        },
-                    },
-                ],
-                enforce: 'pre',
-            },
-            {
-                test: /\.(jpeg|jpg|png|svg?)(\?[a-z0-9=&.]+)?$/,
-                type: 'asset/inline',
-            },
-            {
-                test: /\.(ttf|otf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-                type: 'asset/inline',
-            },
-            {
                 test: /\.(ts|js)x?$/,
                 exclude: /node_modules/,
                 use: [
@@ -55,15 +25,6 @@ const configuration: webpack.Configuration = {
                         options: {
                             transpileOnly: true,
                         },
-                    },
-                    {
-                        loader: 'ifdef-loader',
-                        options: {
-                            env: 'PRODUCTION',
-                        },
-                    },
-                    {
-                        loader: ConditionalLoader,
                     },
                 ],
             },
